e38d1a3f29e88b5ff81690811f5a17d64a321b36
Rune Bak Jacobsen <runebakjacobsen@gmail.com>
2020-01-03 19:47:46 +0100

    Add footer elements and styling

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,15 +1,28 @@
 import React from "react"
+import styled from "styled-components"
+import IconLinkBar from "./icon-link-bar"
 
 const Footer = () => {
   return (
-    <footer>
+    <FooterWrapper>
       <div className="container">
-        © {new Date().getFullYear()}, Built with
+        <IconLinkBar />© {new Date().getFullYear()}, Built with
         {` `}
         <a href="https://www.gatsbyjs.org">Gatsby</a>
       </div>
-    </footer>
+    </FooterWrapper>
   )
 }
+
+const FooterWrapper = styled.footer`
+  margin-top: auto;
+  background-color: #1a202c;
+  color: #a0aec0;
+  padding: 1rem 0;
+  text-align: right;
+  a {
+    color: #a0aec0;
+  }
+`
 
 export default Footer
