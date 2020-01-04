0337964d808449e02eeba716d8f43f62647f28c9
Rune Bak Jacobsen <runebakjacobsen@gmail.com>
2020-01-04 23:52:02 +0100

    Add blog index page

---
+++ src/pages/blog/index.js
@@ -0,0 +1,13 @@
+import React from "react"
+import Layout from "../../components/layout"
+import SEO from "../../components/seo"
+
+const BlogPage = () => (
+  <Layout>
+    <SEO title="Blog" />
+    <h1>Posts</h1>
+    <p>Here is the posts</p>
+  </Layout>
+)
+
+export default BlogPage
