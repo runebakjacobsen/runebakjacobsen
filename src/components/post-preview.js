eb287e3ed01dca403c86925d1b797c5b1c8cbd70
Rune Bak Jacobsen <runebakjacobsen@gmail.com>
2020-01-05 22:35:40 +0100

    Add image support in posts

--- src/components/post-preview.js
+++ src/components/post-preview.js
@@ -1,13 +1,21 @@
 import React from "react"
+import styled from "styled-components"
 
 const PostPreview = ({ post }) => {
   const { title, date } = post.frontmatter
   return (
-    <div>
-      <h4>{title}</h4>
+    <Post>
+      <h3>{title}</h3>
       <p>{post.excerpt}</p>
-    </div>
+    </Post>
   )
 }
+
+const Post = styled.div`
+  border: 1px solid #e2e8f0;
+  border-radius: 0.25rem;
+  width: 50%;
+  margin: 0 auto;
+`
 
 export default PostPreview
