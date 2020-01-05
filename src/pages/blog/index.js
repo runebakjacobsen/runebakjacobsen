8278466deeea2d0dd0fb4bec43dbf6657f730623
Rune Bak Jacobsen <runebakjacobsen@gmail.com>
2020-01-05 22:03:13 +0100

    Add blog query

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -1,12 +1,30 @@
 import React from "react"
 import Layout from "../../components/layout"
 import SEO from "../../components/seo"
+import { graphql } from "gatsby"
 
-const BlogPage = () => (
+export const query = graphql`
+  query BlogQuery {
+    allMdx(limit: 10, sort: { fields: frontmatter___date }) {
+      edges {
+        node {
+          excerpt
+          frontmatter {
+            title
+            date
+          }
+        }
+      }
+    }
+  }
+`
+
+const BlogPage = ({ data }) => (
   <Layout>
     <SEO title="Blog" />
     <h1>Posts</h1>
     <p>Here is the posts</p>
+    {JSON.stringify(data.allMdx.edges)}
   </Layout>
 )
 
