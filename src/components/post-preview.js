e760d665ab419b81eb865c1d865ed03e48ce7d62
Rune Bak Jacobsen <runebakjacobsen@gmail.com>
2020-01-07 19:39:50 +0100

    Add PostPreview styling

--- src/components/post-preview.js
+++ src/components/post-preview.js
@@ -1,15 +1,24 @@
 import React from "react"
 import styled from "styled-components"
 import Img from "gatsby-image"
+import { above } from "../utilities/breakpoints"
 
 const PostPreview = ({ post }) => {
   const { title, date, featuredImage } = post.frontmatter
   return (
     <Post>
-      <Img fluid={featuredImage.childImageSharp.fluid} />
-      <time>{date}</time>
-      <h3>{title}</h3>
-      <p>{post.excerpt}</p>
+      <div>
+        <Img
+          imgStyle={{ objectFit: "contain" }}
+          fluid={featuredImage.childImageSharp.fluid}
+        />
+      </div>
+      <div>
+        <h3>{title}</h3>
+        <p>
+          <time>{date}</time> - {post.excerpt}
+        </p>
+      </div>
     </Post>
   )
 }
@@ -17,8 +26,25 @@ const PostPreview = ({ post }) => {
 const Post = styled.article`
   border: 1px solid #e2e8f0;
   border-radius: 0.25rem;
-  width: 50%;
-  margin: 0 auto;
+  display: flex;
+  flex-wrap: wrap;
+  padding: 1rem;
+
+  > div:first-child {
+    width: 100%;
+    margin: auto;
+    ${above.md`
+    width: 30%;
+    margin-right: 1rem;
+    `}
+
+    + div {
+      margin: auto;
+      ${above.md`
+      width: 67.5%;
+      `}
+    }
+  }
 `
 
 export default PostPreview
--- src/global.js
+++ src/global.js
@@ -25,7 +25,7 @@ const GlobalStyle = createGlobalStyle`
         margin: 0;
         font-weight: 400;
         height:100%;
-        line-height: 1.45;
+        line-height: 1.65;
         color: #1b1b1b;
         font-family: 'Inter', -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen",
           "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue",
@@ -40,29 +40,46 @@ const GlobalStyle = createGlobalStyle`
         flex-direction: column;
     }
 
-    p {margin-bottom: 1.25em; }
+    p {margin-bottom: 1.15rem;}
+
 
     h1, h2, h3, h4, h5 {
-        margin: 2.75rem 0 1rem;
-        font-weight: 400;
-        line-height: 1.15;
+    margin: 2.75rem 0 1.05rem;
+    font-weight: 400;
+    line-height: 1.15;
     }
 
     h1 {
-        margin-top: 0;
-        font-size: 3.815em;
-        letter-spacing: -0.05em;
+    margin-top: 0;
+    font-size: 3.052em;
     }
 
-    h2 {font-size: 3.052em;}
+    h2 {font-size: 2.441em;}
 
-    h3 {font-size: 2.441em;}
+    h3 {font-size: 1.953em;}
 
-    h4 {font-size: 1.953em;}
+    h4 {font-size: 1.563em;}
 
-    h5 {font-size: 1.563em;}
+    h5 {font-size: 1.25em;}
 
-    h6 {font-size: 1.25em;}
+    h1:first-child, 
+    h2:first-child, 
+    h3:first-child, 
+    h4:first-child, 
+    h5:first-child, 
+    h6:first-child {
+        margin-top: 0;
+    }
+    
+    p:last-child, 
+    ul:last-child, 
+    ol:last-child, 
+    dl:last-child, 
+    blockquote:last-child, 
+    pre:last-child, 
+    table:last-child {
+        margin-bottom: 0;
+    }
 
     small, .text_small {font-size: 0.8em;}
     
