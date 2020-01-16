5af3df580deb4c1d57b6bda76c23a8faa94a92da
Rune Bak Jacobsen <runebakjacobsen@gmail.com>
2020-01-16 08:09:07 +0100

    Add check if contact form is empty before submit

--- src/components/contact-form.js
+++ src/components/contact-form.js
@@ -1,29 +1,69 @@
-import React from "react"
+import React, { useState } from "react"
 import { Input } from "../elements/inputs"
 import { Button } from "../elements/buttons"
+import { Alert } from "../elements/alerts"
 import styled from "styled-components"
 import { above } from "../utilities/breakpoints"
 
 const ContactForm = () => {
+  const [fields, setFields] = useState({ name: "", email: "", message: "" })
+  const [error, setError] = useState(false)
+
+  const isEmpty = obj => Object.values(obj).some(x => x === null || x === "")
+
+  const validateInputs = e => {
+    if (isEmpty(fields)) {
+      e.preventDefault()
+      setError(true)
+    }
+  }
   return (
     <Form
       name="contact"
       method="post"
       netlify-honeypot="bot-field"
       data-netlify="true"
+      onSubmit={validateInputs}
     >
       <input type="hidden" name="bot-field" />
 
       <label>Your Name:</label>
-      <Input type="text" name="name" />
+      <Input
+        type="text"
+        name="name"
+        onChange={e => {
+          setError(false)
+          setFields({ ...fields, name: e.target.value })
+        }}
+      />
 
       <label>Your Email:</label>
-      <Input type="email" name="email" />
+      <Input
+        type="email"
+        name="email"
+        onChange={e => {
+          setError(false)
+          setFields({ ...fields, email: e.target.value })
+        }}
+      />
 
       <label>Message:</label>
-      <Input as="textarea" name="message" />
+      <Input
+        as="textarea"
+        name="message"
+        onChange={e => {
+          setError(false)
+          setFields({ ...fields, message: e.target.value })
+        }}
+      />
 
       <Button type="submit">Send message</Button>
+
+      {error && (
+        <Alert>
+          Looks like you forgot to fill out something! Please check again.
+        </Alert>
+      )}
     </Form>
   )
 }
