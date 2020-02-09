2ec4da205f2e7dad1312061ee07189b663e5925c
Rune Bak Jacobsen <runebakjacobsen@gmail.com>
2020-02-09 11:41:17 +0100

    Dynamic h1 from frontmatter in blog posts

--- src/components/posts-layout.js
+++ src/components/posts-layout.js
@@ -3,17 +3,35 @@ import Layout from "./layout"
 import PropTypes from "prop-types"
 import { MDXProvider } from "@mdx-js/react"
 import CodeBlock from "../components/code-block"
+import { Highlight } from "./highlight"
 
 const components = {
   pre: CodeBlock,
 }
 
-const PostsLayout = ({ children }) => {
+const PostsLayout = ({ pageContext, children }) => {
+  const options = {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  }
+
+  const dateObject = new Date(pageContext.frontmatter.date)
+
   return (
     <MDXProvider components={components}>
       <Layout>
         <section>
-          <article>{children}</article>
+          <article>
+            <h1>
+              <Highlight>{pageContext.frontmatter.title}</Highlight>
+            </h1>
+            <p>
+              <time datetime={pageContext.frontmatter.date}></time>
+              {dateObject.toLocaleDateString("en-US", options)}
+            </p>
+            {children}
+          </article>
         </section>
       </Layout>
     </MDXProvider>
